09613acb827c13521b55a42d16321fbf4309a1d3
3415 <3415@example.com>
2020-10-14 12:50:06 +0900

    users入力チェック追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,21 @@
 class User < ApplicationRecord
+    validate :error_check
+    def error_check
+      if user_id.blank?
+        errors[:base] << 'ユーザーIDは必ず入力してください。'
+      end
+  
+      if ps.blank?
+          errors[:base] << 'パスワードは必ず入力してください。'
+      end
+  
+      if name.blank?
+          errors[:base] << '名前は必ず入力してください。'
+      end
+  
+      if authority.blank?
+          errors[:base] << '権限は必ずどちらか押してください。'
+      end
+    end
+  
 end
