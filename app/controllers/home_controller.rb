85c0b0b59b114dc465b5a3ccd811da25bc6c5b8d
3429 <3429@example.com>
2020-10-28 12:47:25 +0900

    グラフ変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -3,7 +3,13 @@ class HomeController < ApplicationController
     @today = Date.today
     week = ['日','月','火','水','木','金','土']
     @yobi = week[@today.wday]
-    @data = ['2019-06-01', 55], ['2019-06-02', 50], ['2019-06-03', 45]
+    entries = Entry.where(user_id: "s20181431")
+
+
+    @data = []
+    entries.each do |entry|
+      @data << [entry.day, entry.weight]
+    end
   end
 
   def calendar_index
