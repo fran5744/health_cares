8b1b8e35d56e7c427f2827455875c931ce3a8529
3429 <3429@example.com>
2020-11-16 12:53:59 +0900

    グラフのリンク

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,15 +4,26 @@ class HomeController < ApplicationController
     week = ['日','月','火','水','木','金','土']
     @yobi = week[@today.wday]
     entries = Entry.where(user_id: session[:user_id])
-    logger.debug("==============")
-    logger.debug(params)
+    period_h = {}
     case params[:period]
     when "week" then
-      entries = entries.where('day <= ?', "2020-10-31")
-      entries = entries.where('day >= ?', "2020-10-25")
+      now_week = Date.today.all_week
+      begin_week = Date.today.beginning_of_week
+      end_week = Date.today.end_of_week
+      entries = entries.where('day >= ?', "#{begin_week}")
+      entries = entries.where('day <= ?', "#{end_week}")
+      now_week.each do |day|
+        period_h.store(day.strftime('%Y-%m-%d'), "")
+      end
     when "month" then
-      entries = entries.where('day <= ?', "2020-10-31")
-      entries = entries.where('day >= ?', "2020-10-01")
+      now_month = Date.today.all_month
+      begin_month = Date.today.beginning_of_month
+      end_month = Date.today.end_of_month
+      entries = entries.where('day <= ?', "#{begin_month}")
+      entries = entries.where('day >= ?', "#{end_month}")
+      now_month.each do |day|
+        period_h.store(day.strftime('%Y-%m-%d'), "")
+      end
     when "3month" then
       entries = entries.where('day <= ?', "2020-10-31")
       entries = entries.where('day >= ?', "2020-07-01")
@@ -25,11 +36,15 @@ class HomeController < ApplicationController
     end
     @user_id = session[:user_id]
     @authority = session[:authority]
-
+    
     @data = []
+
     entries.each do |entry|
-      @data << [entry.day, entry.weight]
+      period_h[entry.day.strftime('%Y-%m-%d')] = entry.weight
     end
+
+    # ハッシュを配列に変換
+    @data = period_h.to_a
     @memo = []
     entries.each do |entry|
       @memo << [entry.day,entry.memo]
@@ -40,3 +55,4 @@ class HomeController < ApplicationController
     @entry = Entry.find_by(user_id: session[:user_id])
   end
 end
+
