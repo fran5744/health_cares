3d6051cbf9402107e4dbd71bd591802bc4140353
3437 <3437@gmail.com>
2020-11-16 11:09:03 +0900

    背景変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,7 +4,7 @@ class PostsController < ApplicationController
   # GET /posts
   # GET /posts.json
   def index
-    @posts = Post.all
+    @posts = Post.where(movie_type: params[:m_type])
   end
 
   # GET /posts/1
