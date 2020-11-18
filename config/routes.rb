03cc74f67426fc5c6048144589cbf3b20df86166
3415 <3415@example.com>
2020-11-18 12:51:52 +0900

    カレンダー情報編集

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,9 @@ Rails.application.routes.draw do
   post 'logins/login_check'
   post 'logins/logout'
 
+  root  to: 'ajax_test#top'
+  post  'ajax_test/update', to: 'ajax_test#update', as: 'ajax_test_update'
+
   resources :entries
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
